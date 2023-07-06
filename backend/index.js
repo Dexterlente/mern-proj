b8bc29370b8c07a5f5694936a6885fc17b612afd
Dexterlente <dexter.lente18@gmail.com>
2023-07-07 04:36:22 +0800

    3

--- backend/index.js
+++ backend/index.js
@@ -22,3 +22,38 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+  const upload = multer({ storage });
+  
+  /* ROUTES WITH FILES */
+  app.post("/auth/register", upload.single("picture"), register);
+  app.post("/posts", verifyToken, upload.single("picture"), createPost);
+  
+  /* ROUTES */
+  app.use("/auth", authRoutes);
+  app.use("/users", userRoutes);
+  app.use("/posts", postRoutes);
+  
+  /* MONGOOSE SETUP */
+  const PORT = process.env.PORT || 6001;
+  mongoose
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  
+      /* ADD DATA ONE TIME */
+      // User.insertMany(users);
+      // Post.insertMany(posts);
+    })
+    .catch((error) => console.log(`${error} did not connect`));
